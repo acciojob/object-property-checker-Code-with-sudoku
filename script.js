327e2e5c6623221b8131c9178ef5247acdcaac58
Code-with-sudoku <137153314+Code-with-sudoku@users.noreply.github.com>
2023-11-21 12:43:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-for(let key in sampleObject){
-    if(key === myKey){
-        return true
-    }else{
-       return false
-    }
-  }
+const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
+ 
+function hasKey(key) {
+  //   write your code here
+       for(let k in sampleObject ){
+		   if(k===key){
+			   return true;
+		   }
+	   }
+	   return false;
+}
+ 
+// Do not change the code below
+const key = prompt("Enter Key.");
+alert(hasKey(key));
